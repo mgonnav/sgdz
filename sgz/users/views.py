cd31bc93dd68e8f9bf3ddd91cd4b6ce109dd7a31
mgonnav <mateo@mgonnav.com>
2023-01-30 19:46:49 -0500

    Set API docs as the default redirect after login

--- sgz/users/views.py
+++ sgz/users/views.py
@@ -39,7 +39,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self, *args, **kwargs):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("api-docs")
 
 
 user_redirect_view = UserRedirectView.as_view()
